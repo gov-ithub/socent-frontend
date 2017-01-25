ca424cd28ff568201bef48da476562e8a6a9f8ba
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2017-01-25 14:06:21 +0000

    more testing

--- src/api/API.test.js
+++ src/api/API.test.js
@@ -11,3 +11,28 @@ describe('Enterprise test', () => {
     expect(TestAPI.getEnterprise()).toBeTruthy();
   });
 });
+
+describe('Caens test', () => {
+  it ('returns Caens object', () => {
+    expect(TestAPI.getCaens()).toBeTruthy();
+  });
+});
+
+describe('Domains test', () => {
+  it ('returns Domains object', () => {
+    expect(TestAPI.getDomains()).toBeTruthy();
+  });
+});
+
+describe('Auth test', () => {
+  it ('doesn\'t auth without a token', () => {
+    expect(TestAPI.isLoggedIn()).toBeFalsy();
+  });
+
+  it ('clears token on logout', () => {
+    TestAPI._setToken('foo');
+    expect(TestAPI._getToken()).toBeTruthy();
+    TestAPI.logout();
+    expect(TestAPI._getToken()).toBeFalsy();
+  });
+});
